0fbf75195d09f6b609cc7e045afbb4784409e3d8
Aditya Jadhav <adityajadhav4055@gmail.com>
2023-10-24 19:07:21 +0530

    workcard changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,19 +18,18 @@ module.exports = {
 
     screens: {
       "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
+      
 
       xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
+    
 
       lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
+      
 
       md: { max: "836px" },
-      // => @media (max-width: 767px) { ... }
-
+     
       sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
+      
       exsm: { max: "380px" },
     },
     extend: {
